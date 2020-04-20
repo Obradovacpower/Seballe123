25a424abcd5369515b8b4aa19cc8e679797e526a
Ermin Obradovac <au592117@uni.au.dk>
2020-04-20 20:06:45 +0200

    Tilføjet ViewPages til kitchen, waiter og receptionist

---
+++ HvaSaaSeb/HvaSaaSeb/Views/Restaurant/Receptionist.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HvaSaaSeb.Views.Restaurant
+{
+    public class ReceptionistModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
