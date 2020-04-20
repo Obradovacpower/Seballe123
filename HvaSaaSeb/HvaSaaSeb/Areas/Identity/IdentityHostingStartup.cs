66669a92ff907adc685d088d2c7dd7ee0c817de3
Ermin Obradovac <au592117@uni.au.dk>
2020-04-20 15:38:30 +0200

    Nystart nystart

---
+++ HvaSaaSeb/HvaSaaSeb/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using HvaSaaSeb.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(HvaSaaSeb.Areas.Identity.IdentityHostingStartup))]
+namespace HvaSaaSeb.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ HvaSaaSeb/HvaSaaSeb/Data/Migrations/20200420125656_Seb.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HvaSaaSeb.Data.Migrations
+{
+    public partial class Seb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
